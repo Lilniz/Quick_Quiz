bad6e22c711cfa4a72a128377a7821ca944491dd
Lizzle <leefboettcher@gmail.com>
2022-08-29 00:59:54 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,25 +80,25 @@ function startQuiz(questions, quizContainer, resultsContainer, submitButton) {
     }, 1000);
     
     function startQuestions(questions, quizContainer){
-        var output = [];
-        var answers = ``;
+        // var output = [];
+        // var answers = ``;
 
-        for (var i = 0; i <questions.length; i++) {
-            answers = [];
-            console.log("working");
-            for(letter in questions[i].answers){
-                answers.push(
-                    '<label>'
-                        + 'input type="radio" name="question'+i+'" value="'+letter+'">'
-                        + letter + ': '
-                        + questions[i].answers[letter]
-                );
-            };
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div>'
-            );
-        };
+        // for (var i = 0; i <questions.length; i++) {
+        //     answers = [];
+        //     console.log("working");
+        //     for(letter in questions[i].answers){
+        //         answers.push(
+        //             '<label>'
+        //                 + 'input type="radio" name="question'+i+'" value="'+letter+'">'
+        //                 + letter + ': '
+        //                 + questions[i].answers[letter]
+        //         );
+        //     };
+        //     output.push(
+        //         '<div class="question">' + questions[i].question + '</div>'
+        //         + '<div class="answers">' + answers.join('') + '</div>'
+        //     );
+        // };
     };
 
     startQuestions(questions, quizContainer);
